04f85e1e726aa92e5e12a4972ad20d2d205ffdac
Sumitra-1810 <80254783+Sumitra-1810@users.noreply.github.com>
2021-03-15 17:54:09 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>online college event</title>
+    <link href= "css/style1.css"type="text/css" rel=stylesheet>
+   <link href="https://fonts.googleapis.com/css2?family=Flamenco&display=swap" rel="stylesheet">
+</head>
+<body>
+<header>
+       
+       <div class="main-content-header">
+           <h1><span class="colorchange">Student Attendance Monitoring System</span></h1>
+           
+           <a href="student-login.php" class="btn btn-full">Login As Student</a>
+           <a href="faculty-login.php" class="btn btn-full">Login As Faculty</a>
+       </div>   
+   </header>
+</body>
+</html>
